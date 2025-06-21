0e92765d5b3d1430985c36f90d0c54ea57988019
Reddington420 <GeraltofRiva@proton.me>
2025-06-21 15:37:44 +0200

    Update GameDataHash.hpp

--- game_files/GameDataHash.hpp
+++ game_files/GameDataHash.hpp
@@ -8,6 +8,7 @@ class GameDataHash
 public:
 	bool m_is_japanese_version;
 	std::array<rage::Obf32, 16> m_data;
+	char new_0104[0x18]; // Obf64, added b3570
 };
-static_assert(sizeof(GameDataHash) == 0x104);
+static_assert(sizeof(GameDataHash) == 0x11C);
 #pragma pack(pop)
